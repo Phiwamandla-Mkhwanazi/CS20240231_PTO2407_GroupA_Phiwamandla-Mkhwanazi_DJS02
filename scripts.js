5d57f0fa813471a8155a4927840415ae4bc3ce86
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-03-23 09:27:26 +0200

    Improve form validation and error handling

--- scripts.js
+++ scripts.js
@@ -15,20 +15,25 @@ form.addEventListener("submit", (event) =>
     const { dividend, divider } = Object.fromEntries(entries);
 
     //Scenario: Validation when values are missing.
-    if (!dividend || !divider) 
+    if (!dividend.trim() || !divider.trim()) 
     {
       result.innerText = "Division not performed. Both values are required in inputs. Try again";
       return;
     }
 
+    //Convert inputs to integers
+    const elDividend = parseInt(dividend, 10);
+    const elDivider = parseInt(divider, 10);
+
     //Scenario: Providing anything that is not a number should crash the program.
-    if (isNaN(dividend) || isNaN(divider)) 
+    if (isNaN(elDividend) || isNaN(elDivider)) 
     {
       throw new Error("Non-numeric input provided.");
     }
 
+    
     //Scenario: An invalid division should log an error in the console.
-    if (Number(divider) === 0) 
+    if (elDivider === 0) 
     {
       console.error("Division not performed. Invalid number provided. Try again");
       result.innerText = "Division not performed. Invalid number provided. Try again";
@@ -36,9 +41,8 @@ form.addEventListener("submit", (event) =>
     }
 
     //Scenario: Dividing numbers result in a whole number / Dividing numbers result in a decimal number.
-    result.innerText = Math.floor(Number(dividend) / Number(divider));  
-        
-    
+    result.innerText = Math.floor(elDividend / elDivider);  
+
   }
   catch(error)
   {
