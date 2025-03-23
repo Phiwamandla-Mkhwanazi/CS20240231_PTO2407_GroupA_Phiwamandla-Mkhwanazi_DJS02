c2524c7855335a83fe2ac563afe538ca517c2fa1
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-03-23 09:01:49 +0200

    Validation when values are missing

--- scripts.js
+++ scripts.js
@@ -15,6 +15,10 @@ form.addEventListener("submit", (event) =>
   const { dividend, divider } = Object.fromEntries(entries);
 
   //Scenario: Validation when values are missing.
+  if (!dividend || !divider) {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    return;
+  }
 
   //Scenario: Providing anything that is not a number should crash the program.
 
