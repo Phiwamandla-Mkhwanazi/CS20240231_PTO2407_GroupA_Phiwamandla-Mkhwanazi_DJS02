77e84d9df08331d2e6b76111386a35e7531823b8
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-03-23 08:48:40 +0200

    Solution Algorithm

--- scripts.js
+++ scripts.js
@@ -1,9 +1,29 @@
+"use strict";
+
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
-});
+form.addEventListener("submit", (event) => 
+  {
+    event.preventDefault();
+
+    //Get input values.
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
+
+   //Scenario: Dividing numbers result in a whole number.
+    result.innerText = dividend / divider;  
+    
+   //Scenario: Dividing numbers result in a decimal number.
+
+
+   //Scenario: Validation when values are missing.
+
+
+   //Scenario: An invalid division should log an error in the console.
+
+
+   //Scenario: Providing anything that is not a number should crash the program.
+
+
+  });
