9d5bf97650309ad5de26e3de73f4683e6a836635
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-03-23 09:09:14 +0200

    Providing anything that is not a number should crash the program

--- scripts.js
+++ scripts.js
@@ -15,12 +15,17 @@ form.addEventListener("submit", (event) =>
   const { dividend, divider } = Object.fromEntries(entries);
 
   //Scenario: Validation when values are missing.
-  if (!dividend || !divider) {
+  if (!dividend || !divider) 
+  {
     result.innerText = "Division not performed. Both values are required in inputs. Try again";
     return;
   }
 
   //Scenario: Providing anything that is not a number should crash the program.
+  if (isNaN(dividend) || isNaN(divider)) 
+  {
+        throw new Error("Non-numeric input provided.");
+  }
 
   //Scenario: An invalid division should log an error in the console.
 
@@ -33,7 +38,8 @@ form.addEventListener("submit", (event) =>
   }
   catch(error)
   {
-    console.error("Error Message: " + error);
+    console.error(error);
+    document.body.innerHTML = "<h1>Something critical went wrong. Please reload the page</h1>";
   }
 
 });
