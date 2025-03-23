5e9621caeaabebd7624ed2667de527ddfaf2cd8b
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-03-23 09:20:38 +0200

    Dividing numbers result in whole numbers

--- scripts.js
+++ scripts.js
@@ -10,36 +10,35 @@ form.addEventListener("submit", (event) =>
   try
   {
   
-  //Get input values.
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
+    //Get input values.
+    const entries = new FormData(event.target);
+    const { dividend, divider } = Object.fromEntries(entries);
 
-  //Scenario: Validation when values are missing.
-  if (!dividend || !divider) 
-  {
-    result.innerText = "Division not performed. Both values are required in inputs. Try again";
-    return;
-  }
+    //Scenario: Validation when values are missing.
+    if (!dividend || !divider) 
+    {
+      result.innerText = "Division not performed. Both values are required in inputs. Try again";
+      return;
+    }
 
-  //Scenario: Providing anything that is not a number should crash the program.
-  if (isNaN(dividend) || isNaN(divider)) 
-  {
-        throw new Error("Non-numeric input provided.");
-  }
+    //Scenario: Providing anything that is not a number should crash the program.
+    if (isNaN(dividend) || isNaN(divider)) 
+    {
+      throw new Error("Non-numeric input provided.");
+    }
 
-  //Scenario: An invalid division should log an error in the console.
-  if (Number(divider) === 0) {
-    console.error("Division not performed. Invalid number provided. Try again");
-    result.innerText = "Division not performed. Invalid number provided. Try again";
-    return;
-  }
+    //Scenario: An invalid division should log an error in the console.
+    if (Number(divider) === 0) 
+    {
+      console.error("Division not performed. Invalid number provided. Try again");
+      result.innerText = "Division not performed. Invalid number provided. Try again";
+      return;
+    }
 
-  //Scenario: Dividing numbers result in a whole number.
-  result.innerText = dividend / divider;  
-      
-  
-  //Scenario: Dividing numbers result in a decimal number.
-  
+    //Scenario: Dividing numbers result in a whole number / Dividing numbers result in a decimal number.
+    result.innerText = Math.floor(Number(dividend) / Number(divider));  
+        
+    
   }
   catch(error)
   {
